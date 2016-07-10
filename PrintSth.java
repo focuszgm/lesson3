1e029a3460f5803fb6fbf88a6a8a772a8a92b67c
focuszgm <821208429@qq.com>
2016-07-10 13:55:13 +0800

    add
    add

---
+++ PrintSth.java
@@ -0,0 +1,14 @@
+package no1;
+
+public class PrintSth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("One");
+		PrintSth a = new PrintSth();
+		a.say();
+	}
+	public void say(){
+		System.out.println("Two");
+	}
+}
